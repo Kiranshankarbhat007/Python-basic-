e3f32f03970dbefbb549fa12ff744418251feef2
Kiran <kiranshankarbhatr@gmail.com>
2019-08-22 15:38:59 +0530

    hangman game

--- hang.py
+++ hang.py
@@ -3,22 +3,25 @@ import random
 words = ['mercedes','hummer','ford','porche','cadillac',
 'mustang','bmw','lamborghini','mahindra','maruthi',
 'kia','mg','chevrolet','audi','jeep','datsun',
-'aston martin','bentley','bugatti','ferrari','fiat',
-'honda','jagur','hyundai','land rover','lexus',
-'maserati','manza','mclaren''mitsubishi','pagani',
-'tata','tesla','suzuki','volvo','renault','rolls royce',
+'astonmartin','bentley','bugatti','ferrari','fiat',
+'honda','jagur','hyundai','landrover','lexus',
+'maserati','manza','mclaren','mitsubishi','pagani',
+'tata','tesla','suzuki','volvo','renault','rollsroyce',
 'volkswagen','nissan']
 
-random.shuffle(words)
-word = list(words[0])
-# print word
+# with open("../../Downloads/words.txt") as f:
+#     words = f.read()
+#     word = words.split(" ")
 
+# random.shuffle(words)
+# word = word[0]
+word = random.choice(words)
+
+gwords = []
 display = []
 display.extend(word)
-# print display
-print ('the word has',len(display),'length')
+print 'the word has',len(display),'letter'
 
-# display = '_'*len(word)
 for i in range (len(word)):
     display[i] = '-'
 
@@ -26,12 +29,20 @@ print (' '.join(display))
 
 count =0
 while count <len(word):
-    guess = raw_input("guess a letter: ")
-    
-    for i in range(len(word)):
-        if word[i] == guess:
-            display[i] = guess
-            count = count +1
-    print (' '.join(display))
 
+    guess = raw_input("guess a letter: ")
+    if guess not in gwords:
+        gwords.append(guess)
+        print (gwords)
+    else:
+        print "u already guessed the letter"
+        print (gwords)
+    
+    if guess not in display:
+        for i in range(len(word)):
+            if word[i] == guess:
+                display[i] = guess
+                count = count +1
+    
+    print (' '.join(display))
 print ("you gussed the word")
