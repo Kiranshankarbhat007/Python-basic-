2859843a64ff2b7c38865d29a75e89ec9119955f
Kiran <kiranshankarbhatr@gmail.com>
2019-08-23 18:38:06 +0530

    used __name__ varialbe

--- hang.py
+++ hang.py
@@ -17,32 +17,40 @@ words = ['mercedes','hummer','ford','porche','cadillac',
 # word = word[0]
 word = random.choice(words)
 
-gwords = []
-display = []
-display.extend(word)
-print 'the word has',len(display),'letter'
+def hang(word):
+    gwords = []
+    display = []
+    display.extend(word)
+    print ('the word has',len(display),'letter')
 
-for i in range (len(word)):
-    display[i] = '-'
+    for i in range (len(word)):
+        display[i] = '-'
 
-print (' '.join(display))
-
-count =0
-while count <len(word):
-
-    guess = raw_input("guess a letter: ")
-    if guess not in gwords:
-        gwords.append(guess)
-        print (gwords)
-    else:
-        print "u already guessed the letter"
-        print (gwords)
-    
-    if guess not in display:
-        for i in range(len(word)):
-            if word[i] == guess:
-                display[i] = guess
-                count = count +1
-    
     print (' '.join(display))
-print ("you gussed the word")
+
+    count =0
+    while count <len(word):
+        # for pyton 2 version     
+        # guess = raw_input("guess a letter: ")
+
+        # for python 3 version
+        guess = str(input("guess a letter"))
+
+        if guess not in gwords:
+            gwords.append(guess)
+            print (gwords)
+        else:
+            print ("u already guessed the letter")
+            print (gwords)
+    
+        if guess not in display:
+            for i in range(len(word)):
+                if word[i] == guess:
+                    display[i] = guess
+                    count = count +1
+    
+        print (' '.join(display))
+    print ("you gussed the word")
+
+if __name__ == '__main__':
+    hang(word)
