5524dd1d3fdac656bda22c305d748f0075b79c12
Kiran <kiranshankarbhatr@gmail.com>
2019-08-27 10:59:55 +0530

    user guess is limited to set of chance

--- hang.py
+++ hang.py
@@ -1,31 +1,31 @@
 import random
 
 def selection():
-    words = ['mercedes','hummer','ford','porche','cadillac',
-    'mustang','bmw','lamborghini','mahindra','maruthi',
-    'kia','mg','chevrolet','audi','jeep','datsun',
-    'astonmartin','bentley','bugatti','ferrari','fiat',
-    'honda','jagur','hyundai','landrover','lexus',
-    'maserati','manza','mclaren','mitsubishi','pagani',
-    'tata','tesla','suzuki','volvo','renault','rollsroyce',
-    'volkswagen','nissan']
+    # word = ['mercedes','hummer','ford','porche','cadillac',
+    # 'mustang','bmw','lamborghini','mahindra','maruthi',
+    # 'kia','mg','chevrolet','audi','jeep','datsun',
+    # 'astonmartin','bentley','bugatti','ferrari','fiat',
+    # 'honda','jagur','hyundai','landrover','lexus',
+    # 'maserati','manza','mclaren','mitsubishi','pagani',
+    # 'tata','tesla','suzuki','volvo','renault','rollsroyce',
+    # 'volkswagen','nissan']
 
-# with open("../../Downloads/words.txt") as f:
-#     words = f.read()
-#     word = words.split(" ")
-    # print word
+    with open("../../Downloads/words.txt") as f:
+        words = f.read()
+        word = words.split(" ")
 
     # random.shuffle(words)
     # word = word[0]
-    word = random.choice(words)
+    word = random.choice(word)
     return word
 word = selection()
-# print word
+
 
 def hang(word):
+    print word
     alpha = ['a','b','c','d','e','f','g','h','i','j',
-        'k','l','m','n','o','p','q','r','s','t',
-        'u','v','w','x','y','z']
+             'k','l','m','n','o','p','q','r','s','t',
+             'u','v','w','x','y','z']
     gwords = []
     display = []
     display.extend(word)
@@ -36,11 +36,16 @@ def hang(word):
 
     print (' '.join(display))
 
-    count =0
+    chance = 0
+    count = 0
+    d = 5
+    f = 5
+    
     while count <len(word):
-        
         guess = raw_input("Enter your guess: ")
         guess = guess.lower()
+        # chance = chance + 1
+        # print chance
         if guess in alpha:
             if guess not in gwords:
                 gwords.append(guess)
@@ -50,14 +55,28 @@ def hang(word):
                 print (gwords)
         else:
             print ('Entered guess is invalid')
+            print (gwords)
     
         if guess not in display:
             for i in range(len(word)):
                 if word[i] == guess:
                     display[i] = guess
                     count = count +1
-    
-        print (' '.join(display))
-    print ("you gussed the word")
-hang(word)
+            print (' '.join(display))
 
+            if guess in display:
+                pass
+            else:
+                chance = chance +1
+                f = f-1
+                print 'your guess is wrong'
+                print 'you have',f,'chance' 
+                        
+        if chance == d:
+            # print "your chancecs are over"
+            print "YOU LOST THE GAME"
+            break
+    else:
+        print ("YOU WON THE GAME ")
+    
+hang(word)
