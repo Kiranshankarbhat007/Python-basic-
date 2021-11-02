9b76ee6aea6246e87245f4e8cfbe0928df65ef4f
Kiranshankarbhat007 <53334157+Kiranshankarbhat007@users.noreply.github.com>
2021-11-02 20:07:08 +0530

    Udated code from py 2 to 3

--- HangMan_Mini.py
+++ HangMan_Mini.py
@@ -1,18 +1,18 @@
 import random
 
 def selection():
-    # word = ['mercedes','hummer','ford','porche','cadillac',
-    # 'mustang','bmw','lamborghini','mahindra','maruthi',
-    # 'kia','mg','chevrolet','audi','jeep','datsun',
-    # 'astonmartin','bentley','bugatti','ferrari','fiat',
-    # 'honda','jagur','hyundai','landrover','lexus',
-    # 'maserati','manza','mclaren','mitsubishi','pagani',
-    # 'tata','tesla','suzuki','volvo','renault','rollsroyce',
-    # 'volkswagen','nissan']
+    word = ['mercedes','hummer','ford','porche','cadillac',
+    'mustang','bmw','lamborghini','mahindra','maruthi',
+    'kia','mg','chevrolet','audi','jeep','datsun',
+    'astonmartin','bentley','bugatti','ferrari','fiat',
+    'honda','jagur','hyundai','landrover','lexus',
+    'maserati','manza','mclaren','mitsubishi','pagani',
+    'tata','tesla','suzuki','volvo','renault','rollsroyce',
+    'volkswagen','nissan']
 
-    with open("../../Downloads/words.txt") as f:
-        words = f.read()
-        word = words.split(" ")
+    # with open("../../Downloads/words.txt") as f:
+    #     words = f.read()
+    #     word = words.split(" ")
 
     # random.shuffle(words)
     # word = word[0]
@@ -22,7 +22,7 @@ word = selection()
 
 
 def hang(word):
-    print word
+    print (word)
     alpha = ['a','b','c','d','e','f','g','h','i','j',
              'k','l','m','n','o','p','q','r','s','t',
              'u','v','w','x','y','z']
@@ -42,7 +42,7 @@ def hang(word):
     f = no_of_chance
     
     while count <len(word):
-        guess = raw_input("Enter your guess: ")
+        guess = input("Enter your guess: ")
         guess = guess.lower()
         if guess in alpha:
             if guess not in gwords:
@@ -67,12 +67,12 @@ def hang(word):
             else:
                 chance = chance +1
                 f = f-1
-                print 'your guess is wrong'
-                print 'you have',f,'chance' 
+                print ('your guess is wrong')
+                print ('you have',f,'chance') 
                         
         if chance == no_of_chance:
             # print "your chancecs are over"
-            print " YOU LOST THE GAME "
+            print (" YOU LOST THE GAME ")
             break
     else:
         print (" YOU WON THE GAME ")
